90f9d1c6cdfd4f1533aa1703724a99687339d975
AnaPicolotto <ana.c.mineiro@aluno.senai.br>
2026-02-23 16:39:52 -0300

    sou otaria

---
+++ script.js
@@ -0,0 +1,23 @@
+
+const btn = document.getElementById('showMessage');
+
+const box = document.getElementById('messageBox');
+
+const messages = [
+'Oie, Mundo!',
+'Boas vindas torcedores',
+'Corinthians está na semi',
+'Iremos ser campeões de novo',
+'Vai Curintia!'
+];
+
+let current = 0;
+
+btn.addEventListener('click', () => {
+
+box.classList.add('show');
+
+box.textContent = messages[current];
+
+current = (current + 1) % messages.length;
+});
